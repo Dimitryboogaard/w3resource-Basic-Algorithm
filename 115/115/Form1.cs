320d5b90a8d112ada8e63b2f3fde532c26921aa5
Dimitry <40239514+Dimitryboogaard@users.noreply.github.com>
2019-07-03 22:58:33 +0200

    Add files via upload

---
+++ 115/115/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _115
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAntwoord_Click(object sender, EventArgs e)
+        {
+            
+            rtUitvoer.Text += test(new[] { 1, 5, 6, 9, 10, 17 }) + Environment.NewLine;
+            rtUitvoer.Text += test(new[] { 1, 5, 5, 5, 10, 17 }) + Environment.NewLine;
+            rtUitvoer.Text += test(new[] { 1, 1, 5, 5, 5, 5 }) + Environment.NewLine;
+
+        }
+
+        static bool test(int[] arrayGetallen)
+        {
+            int intSom = 0;
+
+            foreach(int Getal in arrayGetallen)
+            {
+                if (Getal == 5)
+                    intSom += 5;
+            }
+
+            return intSom == 15;
+        }
+    }
+}
---
+++ 116/116/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _116
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAntwoord_Click(object sender, EventArgs e)
+        {
+            rtUitvoer.Text += test(new[] { 1, 5, 6, 9, 3, 3 }) + Environment.NewLine;
+            rtUitvoer.Text += test(new[] { 1, 5, 5, 5, 10, 17 }) + Environment.NewLine;
+            rtUitvoer.Text += test(new[] { 1, 3, 3, 5, 5, 5 }) + Environment.NewLine;
+
+        }
+
+        static bool test(int[] arrayGetallen)
+        {
+            int int3Teller = 0;
+            int int5Teller = 0;
+
+            foreach(int Getal in arrayGetallen)
+            {
+                if (Getal == 3)
+                    int3Teller++;
+                if (Getal == 5)
+                    int5Teller++;
+            }
+
+            return int3Teller > int5Teller;
+
+        }
+    }
+}
