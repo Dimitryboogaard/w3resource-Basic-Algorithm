7419556202612dffe7e91339fc60fc3820d817f7
Dimitry <40239514+Dimitryboogaard@users.noreply.github.com>
2019-07-03 22:29:27 +0200

    Add files via upload

---
+++ 71/71/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _71
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string strTekst;
+
+        private void btnAntwoord_Click(object sender, EventArgs e)
+        {
+            strTekst = tbTekst.Text;
+
+            if (strTekst.Length % 2 == 0)
+            {
+                lblAntwoord.Text = strTekst.Substring(strTekst.Length / 2 - 1, 2);
+            }
+        }
+    }
+}
