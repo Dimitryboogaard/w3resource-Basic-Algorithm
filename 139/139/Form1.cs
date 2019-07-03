7edf9fc0fd80c7d072e77acd9a7d854e629360fd
Dimitry <40239514+Dimitryboogaard@users.noreply.github.com>
2019-07-03 23:14:30 +0200

    Add files via upload

---
+++ 139/139/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _139
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAntwoord_Click(object sender, EventArgs e)
+        {
+            rtUitvoer.Text += test(123) + "\n";
+            rtUitvoer.Text += test(13) + "\n";
+            rtUitvoer.Text += test(222) + "\n";
+
+        }
+
+        static bool test(int Getal)
+        {
+            while (Getal > 0)
+            {
+                if (Getal % 10 == 2)
+                    return true;
+                Getal /= 10;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ 140/140/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _140
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int[] arrayGetallen = new[] { 1, 2, 3, 5, 7, 9, 10 };
+        int intLengte = 3, intTeller;
+
+        private void btnAntwoord_Click(object sender, EventArgs e)
+        {
+            int[] arrayNieuw = new int[intLengte];
+
+            foreach (int Getal in arrayGetallen)
+            {
+                if (Getal % 2 != 0 && intTeller < intLengte)
+                {
+                    arrayNieuw[intTeller] = Getal;
+                    intTeller++;
+                }
+
+            }
+
+            lblAntwoord.Text = "";
+
+            foreach (int Getal in arrayNieuw)
+            {
+                lblAntwoord.Text += Getal.ToString() + " ";
+            }
+            
+        }
+    }
+}
