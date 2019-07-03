bd0656701b9591f9c2965f9807974af982103015
Dimitry <40239514+Dimitryboogaard@users.noreply.github.com>
2019-07-03 22:11:57 +0200

    Add files via upload

---
+++ 43/43/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _43
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int intGetal;
+
+        private void btnAntwoord_Click(object sender, EventArgs e)
+        {
+            intGetal = Convert.ToInt32(tbGetal.Text);
+
+            if (intGetal < 0)
+                tbGetal.Text = "";
+            else
+                lblAntwoord.Text = Convert.ToString((intGetal % 3 == 0) ^ (intGetal % 7 == 0));
+        }
+    }
+}
