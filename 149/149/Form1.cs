b6daced1c083a0cc22cf66c8ec692e3ce264edab
Dimitry <40239514+Dimitryboogaard@users.noreply.github.com>
2019-07-03 23:20:08 +0200

    Add files via upload

---
+++ 149/149/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _149
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int[] arrayGetallen = new[] { 0, -2, 1, 2, 3, 5, 4, 7, 8 };
+
+        private void btnAntwoord_Click(object sender, EventArgs e)
+        {
+            lblAntwoord.Text = "";
+
+            foreach(int Getal in arrayGetallen)
+            {
+                if (Getal < 4)
+                    lblAntwoord.Text += Getal.ToString() + " ";
+            }
+        }
+    }
+}
---
+++ 150/150/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _150
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int[] arrayGetallen = new[] { 10, 22, 35, 47, 53, 67 };
+
+        private void btnAntwoord_Click(object sender, EventArgs e)
+        {
+            lblAntwoord.Text = "";
+
+            foreach(int Getal in arrayGetallen)
+            {
+                if (Getal % 10 != 7)
+                    lblAntwoord.Text += Getal.ToString() + " ";
+            }
+        }
+    }
+}
