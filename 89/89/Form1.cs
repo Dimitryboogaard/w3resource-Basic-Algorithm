8babbaad6a380d91b2591c03ae7db21de7d45fa2
Dimitry <40239514+Dimitryboogaard@users.noreply.github.com>
2019-07-03 22:41:22 +0200

    Add files via upload

---
+++ 89/89/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _89
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAntwoord_Click(object sender, EventArgs e)
+        {
+            rtUitvoer.Text += test(new[] { 10, 20, 40, 50 }) + Environment.NewLine;
+            rtUitvoer.Text += test(new[] { 10, 20, 40, 10 }) + Environment.NewLine;
+            rtUitvoer.Text += test(new[] { 12, 24, 35, 55 }) + Environment.NewLine;
+
+        }
+
+        public static bool test(int[] arrayGetallen)
+        {
+            return arrayGetallen[0] == arrayGetallen[arrayGetallen.Length - 1];
+        }
+    }
+}
---
+++ 90/90/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _90
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAntwoord_Click(object sender, EventArgs e)
+        {
+            rtUitvoer.Text += test(new[] { 10, 20, 40, 50 }, new[] { 10, 20, 40, 50 }) + Environment.NewLine;
+            rtUitvoer.Text += test(new[] { 10, 20, 40, 50 }, new[] { 10, 20, 40, 5 }) + Environment.NewLine;
+            rtUitvoer.Text += test(new[] { 10, 20, 40, 50 }, new[] { 1, 20, 40, 5 }) + Environment.NewLine;
+
+        }
+
+        public static bool test(int[] arrayGetallen1, int[] arrayGetallen2)
+        {
+            return arrayGetallen1[0] == arrayGetallen2[0] ||
+                   arrayGetallen1[arrayGetallen1.Length - 1] == arrayGetallen2[arrayGetallen2.Length - 1];
+        }
+    }
+}
