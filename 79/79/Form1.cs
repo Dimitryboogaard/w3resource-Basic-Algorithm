02874e016ecef73e012676bed01f3551012a9b42
Dimitry <40239514+Dimitryboogaard@users.noreply.github.com>
2019-07-03 22:35:21 +0200

    Add files via upload

---
+++ 79/79/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _79
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string strTekst;
+
+        private void btnAntwoord_Click(object sender, EventArgs e)
+        {
+            strTekst = tbTekst.Text;
+
+            lblAntwoord.Text = strTekst.Length < 2 ? strTekst : strTekst.Substring(0, strTekst.Length - 2) +
+                               strTekst.Substring(strTekst.Length - 1, 1) +
+                               strTekst.Substring(strTekst.Length - 2, 1);
+        }
+    }
+}
---
+++ 81/81/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _81
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string strTekst;
+
+        private void btnAntwoord_Click(object sender, EventArgs e)
+        {
+            strTekst = tbTekst.Text;
+
+            lblAntwoord.Text = Convert.ToString(strTekst.Substring(0, 2) ==
+                               strTekst.Substring(strTekst.Length - 2, 2));
+                               
+        }
+    }
+}
