b4610e5726a393d4fe15512c14f1b92423472611
Dimitry <40239514+Dimitryboogaard@users.noreply.github.com>
2019-07-03 23:18:18 +0200

    Add files via upload

---
+++ 145/145/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _145
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int[] arrayGetallen = new[] { 1, 2, 3, 4 };
+
+        private void btnAntwoord_Click(object sender, EventArgs e)
+        {
+            lblAntwoord.Text = "";
+
+            foreach(int Getal in arrayGetallen)
+            {
+                lblAntwoord.Text += Convert.ToString((Getal + 2) * 5) + " ";
+            }
+        }
+    }
+}
---
+++ 146/146/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _146
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int[] arrayGetallen = new[] { 10, 22, 35, 41 };
+
+        private void btnAntwoord_Click(object sender, EventArgs e)
+        {
+            lblAntwoord.Text = "";
+
+            foreach(int Getal in arrayGetallen)
+            {
+                lblAntwoord.Text += Convert.ToString(Getal % 10) + " ";
+            }
+        }
+    }
+}
