8ef0c202e99f7efb33914ec5a3cff91ee17aaaa0
Dimitry <40239514+Dimitryboogaard@users.noreply.github.com>
2019-07-03 23:16:26 +0200

    Add files via upload

---
+++ 142/142/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _142
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int[] arrayGetallen = new[] { 1, 2, 3, 4 };
+        int intTeller;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for(intTeller = 0; intTeller < arrayGetallen.Length; intTeller++)
+            {
+                arrayGetallen[intTeller] = arrayGetallen[intTeller] *
+                                           arrayGetallen[intTeller] *
+                                           arrayGetallen[intTeller];
+            }
+
+            lblAntwoord.Text = "";
+
+            foreach(int Getal in arrayGetallen)
+            {
+                lblAntwoord.Text += Getal + " ";
+            }
+        }
+    }
+}
---
+++ 143/143/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _143
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string[] arrayStrings = new[] { "1", "2", "3", "4" };
+
+        private void btnAntwoord_Click(object sender, EventArgs e)
+        {
+            lblAntwoord.Text = "";
+
+            foreach(string String in arrayStrings)
+            {
+                lblAntwoord.Text += "#" + String + "#" + " ";
+            }
+        }
+    }
+}
