72e7a13626f054292d3f270de0a30817cdecb307
Dimitry <40239514+Dimitryboogaard@users.noreply.github.com>
2019-07-03 22:16:00 +0200

    Add files via upload

---
+++ 50/50/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _50
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string strGetal1, strGetal2, strGetal3;
+
+        private void btnAntwoord_Click(object sender, EventArgs e)
+        {
+            strGetal1 = tbGetal1.Text;
+            strGetal2 = tbGetal2.Text;
+            strGetal3 = tbGetal3.Text;
+
+            
+            lblAntwoord.Text = Convert.ToString(strGetal1.Substring(strGetal1.Length - 1) == strGetal2.Substring(strGetal2.Length - 1) ||
+                               strGetal1.Substring(strGetal1.Length - 1) == strGetal3.Substring(strGetal3.Length - 1) ||
+                               strGetal2.Substring(strGetal2.Length - 1) == strGetal3.Substring(strGetal3.Length - 1));
+            
+
+        }
+    }
+}
---
+++ 51/51/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _51
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int intGetal1, intGetal2, intGetal3;
+
+        private void btnAntwoord_Click(object sender, EventArgs e)
+        {
+            intGetal1 = int.Parse(tbGetal1.Text);
+            intGetal2 = int.Parse(tbGetal2.Text);
+            intGetal3 = int.Parse(tbGetal3.Text);
+
+            lblAntwoord.Text = Convert.ToString(intGetal1 == 20 || intGetal2 == 20 || intGetal3 == 20 ||
+                               intGetal1 < intGetal2 && intGetal1 < intGetal3 ||
+                               intGetal2 < intGetal1 && intGetal2 < intGetal3 ||
+                               intGetal3 < intGetal1 && intGetal3 < intGetal2);
+        }
+    }
+}
