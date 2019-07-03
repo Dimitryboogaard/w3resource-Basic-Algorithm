f9b7aa5357f5b752d4f7df759b3d6c993d4b003f
Dimitry <40239514+Dimitryboogaard@users.noreply.github.com>
2019-07-03 22:39:17 +0200

    Add files via upload

---
+++ 85/85/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _85
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string strTekst;
+
+        private void btnAntwoord_Click(object sender, EventArgs e)
+        {
+            strTekst = tbTekst.Text;
+
+            if (strTekst.StartsWith("py"))
+            {
+                lblAntwoord.Text = strTekst;
+            }
+
+            else if (strTekst.Substring(0, 1) == "p")
+            {
+                lblAntwoord.Text = strTekst.Remove(1, 1);
+            }
+
+            else if (strTekst.Substring(1, 1) == "y")
+                lblAntwoord.Text = strTekst.Remove(0, 1);
+
+            else
+                lblAntwoord.Text = strTekst.Substring(2);
+        }
+    }
+}
